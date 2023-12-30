e0f65e28ac0bb92e7bb732838607c990898866b6
Bert Suffys <bert-suffys@hotmail.com>
2023-12-31 00:14:12 +0100

    Terms of agreement, colorfilterbehavior test, starter file

--- production/PollenFX_1.0/starter/script.js
+++ production/PollenFX_1.0/starter/script.js
@@ -1,30 +1,51 @@
 
+/**
+ * Setup
+ */
 let fxManager;
-
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     fxManager = new FXManager();
-    FXManager.devConfig.DEBUG = true;
-
-    let anchor = document.getElementById("my_element");
-    let origin = new CircularEmitterOrigin(0, 0, 40, 40, -1, -1, true, anchor);
-    let emitter = new EmitterShoot(origin, 200, 400, 1000, 500, 2)
-    // Data
-    let defaultData = new ParticleDefaultData(20, 20, origin, 4, 3, true);
-    let cssData = new ParticleCustomCssData("background-color:pink;border:1px solid black;", 30, 500);
-
-    // initialize
-    emitter.addParticleData(cssData);
-    emitter.addParticleData(defaultData);
-
-    fxManager.addEmitter(emitter,"myEmitter");
-
+    fxManager.addEmitter(getEmitter(), "myEmitter");
     start(0);
 });
 
 
+/**
+ * Function emitter creation function
+ */
+function getEmitter() {
+    
+    /* Anchor, origin and emitter */
+    let anchor = document.getElementById("my_element");
+    let origin = new CircularEmitterOrigin(50, 0, 100, 20, -1, -1, true, anchor);
+    let emitter = new EmitterShoot(origin, 400, 2000, 700, 1000, 2);
+
+    /* Data */
+    const defaultData = new ParticleDefaultData(14, 7, origin, 1, 1, true);
+    const cssData = new ParticleCustomCssData("background-color:red;border:1px solid black; border-radius:5px;", 30, 500);
+    const directionData = new ParticleDirectionData(90, 7, 50, 4);
+    emitter.addParticleData(cssData);
+    emitter.addParticleData(defaultData);
+    emitter.addParticleData(directionData);
+
+    /* Behavior */
+    const directionBehavior = new ParticleDirectionalBehavior()
+    const gravityBehavior = new ParticleGravityBehavior(10, 4)
+    const sizeByLifeBehavior = new ParticleSizeByLifeBehavior([0,1,0])
+    const rotationByDirectionBehavior = new ParticleRotationByDirectionBehavior()
+    emitter.addParticleBehavior(directionBehavior);
+    emitter.addParticleBehavior(gravityBehavior);
+    emitter.addParticleBehavior(sizeByLifeBehavior);
+    emitter.addParticleBehavior(rotationByDirectionBehavior);
+
+    return emitter;
+}
 
 
-function start(runtimeInMs){
+/**
+ * Core logic loop
+ */
+function start(runtimeInMs) {
     fxManager.act(runtimeInMs);
     window.requestAnimationFrame(this.start);
 }
