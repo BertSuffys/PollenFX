0392e86ebba3b2070f38e0a1b66cd47e1b73e924
Bert Suffys <bert-suffys@hotmail.com>
2023-12-31 12:24:57 +0100

    Added sparks to the campfire in the demo

--- demo_v1.0/script.js
+++ demo_v1.0/script.js
@@ -3,19 +3,45 @@ let fxManager;
 FXManager.devConfig.DEBUG = false;
 
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     fxManager = new FXManager(false)
 
-    /* 1. Ads embers */
-    fxManager.addEmitter(getEmbers(), "embers")
-    /* 2. Ads light smoke */
-    fxManager.addEmitter(getLightSmoke(), "light_smoke")
+    /* 1. Adds embers */
+    fxManager.addEmitter(getEmbers(), "embers");
+    /* 2. Adds light smoke */
+    fxManager.addEmitter(getLightSmoke(), "light_smoke");
+    /* 3. Adds sparks */
+    fxManager.addEmitter(getSparks(), "sparks");
 
     start(0);
 });
 
 
-function getLightSmoke(){
+
+function getSparks() {
+    // Tools
+    let anchor = document.getElementById("logWrapper");
+    let origin = new CircularEmitterOrigin(180, 80, 180, 100, -1, -1, true, anchor);
+    let emitter = new EmitterBurst(origin, 10, -1, -1, 400, 5000, 2, 2000)
+    // Data
+    let cssData = new ParticleCustomCssData("background-color:#ff6f21; mix-blend-mode:screen; border-radius:30px; box-shadow: 0px 0px 24px 0px rgba(255,111,33,0.9);", 10, 30);
+    let defaultData = new ParticleDefaultData(3, 2, origin, 0.5, 0.5, true);
+    let directionData = new ParticleDirectionData(90, 6, 180, 10)
+    emitter.addParticleData(defaultData);
+    emitter.addParticleData(cssData);
+    emitter.addParticleData(directionData);
+    // Behavior
+    let directionBehavior = new ParticleDirectionalBehavior()
+    let gravityBehavior = new ParticleGravityBehavior(60, 5)
+    let rotationByDirectionBehavior = new ParticleRotationByDirectionBehavior()
+    emitter.addParticleBehavior(directionBehavior);
+    emitter.addParticleBehavior(gravityBehavior);
+    emitter.addParticleBehavior(rotationByDirectionBehavior);
+
+    return emitter
+}
+
+function getLightSmoke() {
     // Tools
     let anchor = document.getElementById("logWrapper");
     let origin = new CircularEmitterOrigin(200, 110, 140, 100, -1, -1, true, anchor);
@@ -30,11 +56,11 @@ function getLightSmoke(){
     emitter.addParticleData(rotationData);
     emitter.addParticleData(flipbookData);
     // Behavior
-    const opacityByLifeBehavior = new ParticleOpacityByLifeBehavior([0,0.01,0.1,0.4,0]);
-    const sizeByLifeBehavior = new ParticleSizeByLifeBehavior([0.5,1,1.3]);
+    const opacityByLifeBehavior = new ParticleOpacityByLifeBehavior([0, 0.01, 0.1, 0.4, 0]);
+    const sizeByLifeBehavior = new ParticleSizeByLifeBehavior([0.5, 1, 1.3]);
     const flipbookBehavior = new ParticleFlipbookBehavior(100)
     const directionBehavior = new ParticleDirectionalBehavior()
-    const colorFilterBehavior = new ParticleColorfilterBehavior([new Color("#eb5f1a") ,new Color('#888833')])
+    const colorFilterBehavior = new ParticleColorfilterBehavior([new Color("#eb5f1a"), new Color('#888833')])
     emitter.addParticleBehavior(opacityByLifeBehavior);
     emitter.addParticleBehavior(flipbookBehavior);
     emitter.addParticleBehavior(sizeByLifeBehavior);
@@ -46,7 +72,7 @@ function getLightSmoke(){
 
 
 
-function getEmbers(){
+function getEmbers() {
     // Tools
     let anchor = document.getElementById("logWrapper");
     let origin = new CircularEmitterOrigin(200, 110, 140, 100, -1, -1, true, anchor);
@@ -59,7 +85,7 @@ function getEmbers(){
     emitter.addParticleData(cssData);
     emitter.addParticleData(rotationData);
     // Behavior
-    const opacityByLifeBehavior = new ParticleOpacityByLifeBehavior([0,0.6,0], -1, 1500);
+    const opacityByLifeBehavior = new ParticleOpacityByLifeBehavior([0, 0.6, 0], -1, 1500);
     emitter.addParticleBehavior(opacityByLifeBehavior);
     // Return
     return emitter
